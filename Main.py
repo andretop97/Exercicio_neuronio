84707acb0c66d2107f369feadc217964c6c9c572
andre luiz branco barbosa <Andretop97>
2019-06-06 18:07:07 -0300

    final exercicio

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import math
+from sklearn.metrics import mean_squared_error
 
 
 def plotar(w1,w2,bias,title):
@@ -42,11 +43,19 @@ def soma_escalar(vetor, const):
     return novo_vet
 
 
-def soma_vetorial(Vetor1,Vetor2):
-    vetor_resultante =[0]*len(Vetor1)
+def soma_vetorial(Vetor1, Vetor2):
+    vetor_resultante = [0]*len(Vetor1)
     for i in range(len(Vetor1)):
         vetor_resultante[i] = Vetor1[i] + Vetor2[i]
     return vetor_resultante
+
+
+def subtracao_vetorial(Vetor1, Vetor2):
+    vetor_resultante = [0] * len(Vetor1)
+    for i in range(len(Vetor1)):
+        vetor_resultante[i] = Vetor1[i] - Vetor2[i]
+    return vetor_resultante
+
 
 def mul_escalar(Vetor1, const):
     vetor_resultante = [0]*len(Vetor1)
@@ -65,51 +74,56 @@ def mul_ele_vetor(Vetor1,Vetor2):
 def conta_erro(erro):
     numero_erro = 0
     for i in erro:
-        if i != 0 :
             numero_erro = numero_erro + 1
     return  numero_erro
 
 
 def erro_quadratico_medio(Erro):
-    return pow(sum(Erro),2)/len(Erro)
+    MSE = 0
+    for i in Erro:
+        MSE = MSE + i**2
+    MSE = MSE/len(Erro)
+    return MSE
 
 
 def adaline(max_it, Epsilon, alpha, X, d):
-    w = [random.choice([0,1]) for i in range(len(X[0]))]
-    b = random.choice([0, 1])
+    w = [random.random() for i in range(len(X[0]))]
+    b = random.random()
     t = 1
+    e = [0 for x in range(len(X))] # [1]*len(X)
     validacao = True
     while validacao:
-        e = []
+        MSEi = erro_quadratico_medio(e)
         for i in range(len(X)):
             y = funcao_tgH(reta(w, X[i], b))
-            e.append(d[i]-y)
-            w = soma_vetorial(w,mul_escalar(X[i],e[i]*alpha))
+            e[i] = d[i] - y
+            w = soma_vetorial(w, mul_escalar(X[i], e[i]*alpha))
             b = b + alpha * e[i]
-        MSE = erro_quadratico_medio(e)
+        MSEf = erro_quadratico_medio(e)
         t = t + 1
-        print(MSE)
-        validacao = t < max_it and MSE > Epsilon
-    return (w, b)
+        delta_MSE = abs(MSEf-MSEi)
+        validacao = t < max_it and delta_MSE > Epsilon
+    print(f'Iterações: {t}, Delta_MSE: {delta_MSE}')
+    return w, b
 
 
-
-def perceptron(max_it , E, alpha, X, d):
-        w = [random.choice([0,1]) for i in range(len(X[0]))]
-        b = random.choice([0,1])
+def perceptron(max_it, E, alpha, X, d):
+        w = [random.random() for i in range(len(X[0]))]
+        b = random.random()
 
         t = 1
-        while t < max_it and E > 0 :
+        while t < max_it and E > 0:
             e = []
             for i in range(len(X)):
-                y =funcao_ativacao(reta(w,X[i],b))
+                y = funcao_ativacao(reta(w, X[i], b))
                 e.append(d[i] - y)
-                w = soma_vetorial(w,mul_escalar(X[i],e[i]*alpha))
+                w = soma_vetorial(w, mul_escalar(X[i], e[i]*alpha))
                 b = b + (alpha * e[i])
             E = conta_erro(e)
             t = t + 1
-            print(w,b)
-        return(w,b)
+            print(w, b)
+        return w,b
+
 
 def teste():
     teste1 =[1,2,3]
@@ -132,8 +146,8 @@ def main():
     plotar(w[0],w[1],bias,"Porta lógica AND com Adaline")
     
     # Implemente a função Percepton que deve retornar o vetor de pesos e o bias, respectivamente.
-    #w, bias = perceptron(max_it=100, E=1, alpha=.1, X=X, d=d)
-    #plotar(w[0],w[1],bias,"Porta lógica AND com Perceptron")
+    # w, bias = perceptron(max_it=100, E=1, alpha=.1, X=X, d=d)
+    # plotar(w[0],w[1],bias,"Porta lógica AND com Perceptron")
 
 if __name__ == '__main__':
     main()
